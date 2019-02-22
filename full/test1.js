981f814e328524916fc702dd50f8f4c2e320338c
Bruno Ferreira <bmibferreira@gmail.com>
2019-02-22 18:39:38 +0000

    Update test1.js

--- full/test1.js
+++ full/test1.js
@@ -5,6 +5,8 @@ define( [
 // Multifunctional method to get and set values of a collection
 // The value/s can optionally be executed if it's a function
 var access = function( elems, fn, key, value, chainable, emptyGet, raw ) {
+	
+	var z = 123;
 	var i = 0,
 		len = elems.length,
 		bulk = key == null;
